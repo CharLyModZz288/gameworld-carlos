953201ae843208d1cd6e38a4f67ed3a9199a783e
CharLyModZz288 <126071634+CharLyModZz288@users.noreply.github.com>
2026-04-20 19:56:42 +0200

    modificaciones

--- js/connection.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-const supabaseUrl = "https://vforasnmcipqpqwdkygm.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmb3Jhc25tY2lwcXBxd2RreWdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MTIzMTYsImV4cCI6MjA3ODE4ODMxNn0.wP71pAkOFJ8YYNNN7lIRfSrJloqKFsKq3bIjphWBqFc";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
